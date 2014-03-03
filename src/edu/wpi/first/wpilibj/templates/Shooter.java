8131147c254026c1f7ee6e8b7022886cfe5cd5c5
tsteward <tsteward0@gmail.com>
2014-03-02 18:26:47 -0600

    Better Shooter Timeout value

--- src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -90,7 +90,7 @@ public class Shooter implements Runnable
             }
             
             long startLoad = System.currentTimeMillis();
-            while(!LimitSwitch.get() && (System.currentTimeMillis() - startLoad < 5000))
+            while(!LimitSwitch.get() && (System.currentTimeMillis() - startLoad < 500)) //TODO update this to a value that makes more sense with testing
             {
                 WinchMotor1.set(-1);
                 WinchMotor2.set(-1);
@@ -100,7 +100,7 @@ public class Shooter implements Runnable
             WinchMotor1.set(0);
             WinchMotor2.set(0);
             
-            DriverMessages.displayShooterTimeout(System.currentTimeMillis() - startLoad > 5000);
+            DriverMessages.displayShooterTimeout(System.currentTimeMillis() - startLoad > 500); //TODO update this one to match
             
             Loading = false;
             Loaded = true;
