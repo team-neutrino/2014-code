2bb7b619f14b4b422f282d59e2ecb487b04f7de0
teamneutrino <neutrinofrc@gmail.com>
2014-03-13 12:23:27 -0500

    remove camera

--- src/edu/wpi/first/wpilibj/templates/Camera.java
+++ src/edu/wpi/first/wpilibj/templates/Camera.java
@@ -17,60 +17,60 @@ import edu.wpi.first.wpilibj.image.ColorImage;
  */
 public class Camera 
 {
-    private AxisCamera RobotCamera;
-    private Solenoid CameraLight;
+//    private AxisCamera RobotCamera;
+//    private Solenoid CameraLight;
     private DriverMessages DriverMessages;
     
     public Camera(DriverMessages driverMessages)
     {
-        RobotCamera = AxisCamera.getInstance();
-        CameraLight = new Solenoid(MainConstants.CAMERA_LIGHT_SLOT, MainConstants.CAMERA_LIGHT_CHANNEL);
+//        RobotCamera = AxisCamera.getInstance();
+//        CameraLight = new Solenoid(MainConstants.CAMERA_LIGHT_SLOT, MainConstants.CAMERA_LIGHT_CHANNEL);
         DriverMessages = driverMessages;
     }
     
     public boolean goalIsHot()
     {
-        CameraLight.set(true);
-        int tapes;
-        long startTime = System.currentTimeMillis();
-        while(!RobotCamera.freshImage() && (System.currentTimeMillis() - startTime < 1000))
-        {
-            try
-            {
-                Thread.sleep(1);
-            } catch (InterruptedException ex)
-            {
-                ex.printStackTrace();
-            }
-        }
-        try 
-        {
-            ColorImage image = RobotCamera.getImage();
-            image.write("color.bmp");
-            //red green blue
-            //BinaryImage thresholdImage = image.thresholdRGB(0, 64, 128, 255, 0, 64); //green threshold
-            BinaryImage thresholdImage = image.thresholdRGB(0, 128, 192, 255, 192, 255); //blue threshold
-            thresholdImage.write("threshold.bmp");
-            BinaryImage rmvpartImage = thresholdImage.removeSmallObjects(true, 3);
-            rmvpartImage.write("rmpart.bmp");
-            //System.out.println(rmvpartImage.getNumberParticles()); 
-            tapes = rmvpartImage.getNumberParticles();
-            image.free();
-            thresholdImage.free();
-            rmvpartImage.free();
-        }
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
+//        CameraLight.set(true);
+//        int tapes;
+//        long startTime = System.currentTimeMillis();
+//        while(!RobotCamera.freshImage() && (System.currentTimeMillis() - startTime < 1000))
+//        {
+//            try
+//            {
+//                Thread.sleep(1);
+//            } catch (InterruptedException ex)
+//            {
+//                ex.printStackTrace();
+//            }
+//        }
+//        try 
+//        {
+//            ColorImage image = RobotCamera.getImage();
+//            image.write("color.bmp");
+//            //red green blue
+//            //BinaryImage thresholdImage = image.thresholdRGB(0, 64, 128, 255, 0, 64); //green threshold
+//            BinaryImage thresholdImage = image.thresholdRGB(0, 128, 192, 255, 192, 255); //blue threshold
+//            thresholdImage.write("threshold.bmp");
+//            BinaryImage rmvpartImage = thresholdImage.removeSmallObjects(true, 3);
+//            rmvpartImage.write("rmpart.bmp");
+//            //System.out.println(rmvpartImage.getNumberParticles()); 
+//            tapes = rmvpartImage.getNumberParticles();
+//            image.free();
+//            thresholdImage.free();
+//            rmvpartImage.free();
+//        }
+//        catch (Exception ex) 
+//        {
+//            ex.printStackTrace();
             DriverMessages.displayCameraError(true);
             DriverMessages.displayHot(true);
             return true;
-        }
-        
-        CameraLight.set(false);
-        
-        DriverMessages.displayCameraError(false);
-        DriverMessages.displayHot(false);
-        return (tapes == 2);
+//        }
+//        
+//        CameraLight.set(false);
+//        
+//        DriverMessages.displayCameraError(false);
+//        DriverMessages.displayHot(false);
+//        return (tapes == 2);
     }
 }
