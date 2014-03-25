5e8db47716ac7e49d89410c7f558e851d5283792
teamneutrino <neutrinofrc@gmail.com>
2014-03-24 19:02:20 -0500

    tempory removal of beambreak for testing

--- src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -165,21 +165,21 @@ public class Shooter implements Runnable
             //cock
             BeamBreakPower.set(true);
             long startLoad = System.currentTimeMillis();
-            while(!LimitSwitch.get() && BeamBreak.get() && (System.currentTimeMillis() - startLoad < 3000))
+            while(!LimitSwitch.get() /*&& BeamBreak.get()*/ && (System.currentTimeMillis() - startLoad < 3000))
             {
-                WinchMotor1.set(1);
-                WinchMotor2.set(1);
+                WinchMotor1.set(.7); //1
+                WinchMotor2.set(.7); //1
                 Thread.sleep(5);
                 //System.out.println("Cocking: " + (System.currentTimeMillis() - startLoad));
             }
             
-            while(!LimitSwitch.get() && !BeamBreak.get() && (System.currentTimeMillis() - startLoad < 10000))
-            {
-                WinchMotor1.set(.3);
-                WinchMotor2.set(.3);
-                Thread.sleep(1);
-                //System.out.println("Cocking: " + (System.currentTimeMillis() - startLoad));
-            }
+//            while(!LimitSwitch.get() && !BeamBreak.get() && (System.currentTimeMillis() - startLoad < 10000))
+//            {
+//                WinchMotor1.set(.3);
+//                WinchMotor2.set(.3);
+//                Thread.sleep(1);
+//                //System.out.println("Cocking: " + (System.currentTimeMillis() - startLoad));
+//            }
             
             WinchMotor1.set(0);
             WinchMotor2.set(0);
